43ea4b653a8de436cf327ea4415a8afb4938e4c2
vishu311 <vishal.311gupta@gmail.com>
2021-04-02 22:14:37 +0530

    change nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,11 +7,14 @@ const Navbar = () => {
         <div className = "container-fluid">
             <div className="row">
                 <div className="col-10 mx-auto">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <div className="container-fluid nav_bg">
+                    <nav className="navbar navbar-expand-lg navbar-light">
+                        <div className="container-fluid ">
                             <NavLink className="navbar-brand" to="/">StreetFooders</NavLink>
-                                <div className="collapse navbar-collapse justify-content-center p-2" id="navbarSupportedContent">
-                                    <ul className="navbar-nav">
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                                    <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                                         <li className="nav-item">
                                         <NavLink activeClassName="menu_active" 
                                         exact className="nav-link active" 
@@ -30,6 +33,7 @@ const Navbar = () => {
                                 </div>
                 </div>
                 </nav>
+                
 </div>
 </div>
 </div>
